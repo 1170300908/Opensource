2abf28183354857616b789a403b2982484bd04f1
1171000322 <772875964@qq.com>
2019-07-07 15:53:10 +0800

    suggestion

--- src/poet/Main.java
+++ src/poet/Main.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final GraphPoet nimoy = new GraphPoet(new File("src/poet/mugar-omni-theater.txt"));
         final String input = "Test the system.";
-        System.out.println(input + "\n>>>\n" + nimoy.poem(input));
+        System.out.println(input + "\n>>>>\n" + nimoy.poem(input));
     }
     
 }
